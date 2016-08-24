c89c7aca79cc3675c641a2924df88df8fd63acb2
Sowmya Kambam <sowmya.kambam@gramener.com>
2016-08-24 11:50:53 +0000

    Replace staticscrape.py

--- staticscrape.py
+++ staticscrape.py
@@ -1,6 +1,5 @@
 import mechanize
 from bs4 import BeautifulSoup
-import os
 import csv
 
 #using mechanize for authentication
@@ -8,20 +7,22 @@ br = mechanize.Browser()
 br.open('https://supremo.nic.in/knowyourofficerIAs.aspx')
 
 br.select_form(nr=0)
-form=br.form
+form = br.form
 
 # years=['1990','1991','1992',]
-years=[]
-for i in range(1999,2008):
-    years.append(str(i))
-officerdata=[]
-i=""
+allotyearstart=1900
+allotyrend=2017
+years = []
+for i in range(allotyearstart,allotyrend):
+    years.append("%s" % i)
+officerdata = []
+i = ""
 for i in range(len(years)):
     # print type(years[i])
-    str=years[i]
+    str = years[i]
 
-    form["txtAllotYr"]=str
-    br.form=form
+    form["txtAllotYr"] = str
+    br.form = form
     res = br.submit(name='btnSubmit')
     final = res.geturl()
     print res.geturl()+""
@@ -35,8 +36,8 @@ for i in range(len(years)):
 
 # print officerdata
 
-csv_columns = ['Name','Service','Cadre','Allotment year','Date of Birth','Date of Joining']
-with open("iasoffic.csv", "wb") as output:
+csv_columns = ['Name', 'Service', 'Cadre', 'Allotment year', 'Date of Birth', 'Date of Joining']
+with open("iasofficerdata.csv", "wb") as output:
 
     writer = csv.writer(output)
     writer.writerow(csv_columns)
